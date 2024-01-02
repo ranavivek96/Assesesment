9c8cd9fa362d99365fe2721cf1cfa5cde17f5cd7
Vivek Rana <142773693+ranavivek96@users.noreply.github.com>
2024-01-02 19:57:50 +0530

    Add files via upload

--- MySQL Project (Pizza Sales) 02-01-2024.sql
+++ MySQL Project (Pizza Sales) 02-01-2024.sql
@@ -1,8 +1,8 @@
 /*  ========================================================================    */
 			-- Project #4 --
-			-- Databse Queries using MySQL --
+	-- Databse Queries using MySQL --
 /*  ========================================================================    */
-			-- Submitted by: Rana Vivekkumar R. --
+	-- Submitted by: Rana Vivekkumar R. --
 			-- Course: Data Science --
 /*  ========================================================================    */
 			/* Problem Statement Pizza Sales */
@@ -57,40 +57,40 @@ from pizza_sales;
 -- 6. Show Daily trend for total Orders. 
 -- Identify any patterns or fluctuations in order volumes on a daily basis.
 select count(order_id) as "Order per Day", 
-	order_date as "Order Date"
-	from pizza_sales
-	group by order_date;
+order_date as "Order Date"
+from pizza_sales
+group by order_date;
 
 -- 7. Show Monthly Trend for Total Orders. 
 -- Identify peak hours or periods of high order activity.
 select count(order_id) as "Customer Orders", 
-		order_time as "Order Time", 
-        order_date as "Order Date" 
-        from pizza_sales
-        group by order_time, order_date;
+order_time as "Order Time", 
+order_date as "Order Date" 
+from pizza_sales
+group by order_time, order_date;
 
 -- 8. Show Percentage of Sales by Pizza Category. 
 -- Identify that popularity of various pizza categories and their contribution to overall sales.
 select substr(avg(total_price), 1,5) as "Percentage of Sales",
-		count(quantity) as "No. of Pizza's", 
-		pizza_category as "Pizza Category" 
-        from pizza_sales
-        group by pizza_category;
+count(quantity) as "No. of Pizza's", 
+pizza_category as "Pizza Category" 
+from pizza_sales
+group by pizza_category;
 
 -- 9. Show Percentage of Sales by Pizza Size. 
 -- Understand customer preferences for pizza sizes and their impact on sales.
 select substr(avg(total_price), 1,5) as "Percentage of Sales(%)",
-		count(quantity) as "No. of Pizza's", 
-		pizza_size as "Pizza Size" 
-        from pizza_sales
-        group by pizza_size;
+count(quantity) as "No. of Pizza's", 
+pizza_size as "Pizza Size" 
+from pizza_sales
+group by pizza_size;
 
 -- 10. Show Total Pizza Sold by Pizza Category. 
 -- Compare the sales performance of different pizza categories.
 select count(quantity) as "Total Pizza Sold", 
-		pizza_category as "Pizza Category" 
-        from pizza_sales
-        group by pizza_category;
+pizza_category as "Pizza Category" 
+from pizza_sales
+group by pizza_category;
 
 -- 11. Show Top 5 Best Sellers by Revenue, Total Quantity and Total Orders. 
 -- Identify the most popular pizza options.
@@ -99,13 +99,13 @@ select pizza_name as "Pizza Name",sum(quantity) as "Total Quantity" from pizza_s
 select pizza_name as "Pizza Name",count(order_id) as "Total Orders" from pizza_sales group by pizza_name order by count(order_id) desc limit 5;
 
 select pizza_name as "Pizza Name", 
-		sum(total_price) as "Revenue", 
-		sum(quantity) as "Total Quantity", 
-		count(order_id) as "Total Orders" 
-        from pizza_sales
-        group by pizza_name 
-        order by sum(total_price) 
-        desc limit 5;
+sum(total_price) as "Revenue", 
+sum(quantity) as "Total Quantity", 
+count(order_id) as "Total Orders" 
+from pizza_sales
+group by pizza_name 
+order by sum(total_price) 
+desc limit 5;
 
 -- 12. Bottom 5 Best Sellers by Revenue, Total Quantity and Total Orders.
 -- Identify underperforming or less popular pizza options.
@@ -114,12 +114,12 @@ select pizza_name as "Pizza Name",sum(quantity) as "Total Quantity" from pizza_s
 select pizza_name as "Pizza Name",count(order_id) as "Total Orders" from pizza_sales group by pizza_name order by count(order_id) limit 5;
 
 select pizza_name as "Pizza Name", 
-		sum(total_price) as "Revenue", 
-		sum(quantity) as "Total Quantity", 
-        count(order_id) as "Total Orders" 
-        from pizza_sales
-        group by pizza_name 
-        order by sum(total_price) limit 5;
+sum(total_price) as "Revenue", 
+sum(quantity) as "Total Quantity", 
+count(order_id) as "Total Orders" 
+from pizza_sales
+group by pizza_name 
+order by sum(total_price) limit 5;
         
 /* =========================== THE END =========================== */
 
