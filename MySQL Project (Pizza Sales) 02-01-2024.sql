aca66f03cab10d082d9bf45d06f98046ff4f8c2c
Vivek Rana <142773693+ranavivek96@users.noreply.github.com>
2024-01-02 19:51:37 +0530

    Add files via upload

--- MySQL Project (Pizza Sales) 02-01-2024.sql
+++ MySQL Project (Pizza Sales) 02-01-2024.sql
@@ -1,11 +1,11 @@
 /*  ========================================================================    */
-							-- Project #4 --
-					-- Databse Queries using MySQL --
+					-- Project #4 --
+			-- Databse Queries using MySQL --
 /*  ========================================================================    */
-                  -- Submitted by: Rana Vivekkumar R. --
-						-- Course: Data Science --
+			-- Submitted by: Rana Vivekkumar R. --
+				-- Course: Data Science --
 /*  ========================================================================    */
-					/* Problem Statement Pizza Sales */
+			/* Problem Statement Pizza Sales */
 /*  ========================================================================    */
 -- Questions to Solve:
 -- 1. Show Total Revenue. 
@@ -57,9 +57,9 @@ from pizza_sales;
 -- 6. Show Daily trend for total Orders. 
 -- Identify any patterns or fluctuations in order volumes on a daily basis.
 select count(order_id) as "Order per Day", 
-		order_date as "Order Date" 
-        from pizza_sales
-        group by order_date;
+order_date as "Order Date"
+from pizza_sales
+group by order_date;
 
 -- 7. Show Monthly Trend for Total Orders. 
 -- Identify peak hours or periods of high order activity.
